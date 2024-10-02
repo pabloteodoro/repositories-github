dfa79ac673c6fe9f8be0abe307718fbe456fa9ea
Pablo Teodoro <pabloteadoro123@gmail.com>
2024-10-02 00:53:34 -0300

    add packages and configure components
    add new packages, configure api, new components and add yarn in aplication

--- repositories-github/src/pages/Main/index.js
+++ repositories-github/src/pages/Main/index.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { FaGithub, FaPlus } from 'react-icons/fa';
 import { Container, Form, SubmitButton } from './styles';
+import api from '../../services/api';
 
+export default function Main() {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositorio, setRepositorio] = useState([]);
 
-export default function Main(){
-  return(
+  const handleSubmit = useCallback((e) => {
+    e.preventDefault();
+
+    async function submit() {
+      const response = await api.get(`repos/${newRepo}`);
+
+      const data = {
+        name: response.data.name,
+      };
+
+      setRepositorio([...repositorio, data]);
+      setNewRepo('');
+    }
+
+    submit();
+  }, [newRepo, repositorio]);
+
+  return (
     <Container>
-      
       <h1>
-        <FaGithub size={25}/>
-        Meus Repositorios
+        <FaGithub />
+        Repositórios
       </h1>
 
-      <Form onSubmit={()=>{}}>
-        <input type="text" placeholder="Adicionar Repositorios"/>
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Adicionar repositório"
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+        />
 
         <SubmitButton>
-          <FaPlus color="#FFF" size={14}/>
+          <FaPlus color="#FFF" size={14} />
         </SubmitButton>
-
       </Form>
-
     </Container>
-  )
+  );
 }
